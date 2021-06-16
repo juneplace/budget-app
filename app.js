dfb509c21981ffa22a29050569a240fa93b1aa8a
juneplace <52162532+juneplace@users.noreply.github.com>
2021-06-16 14:18:45 +0200

    making app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 class UI {
     constructor() {
         this.budgetFeedback = document.querySelector('.budget-feedback');
@@ -29,7 +30,6 @@ class UI {
             this.budgetInput.value = '';
             this.showBalance();
         }
-
     }
     showBalance() {
         const expense = this.totalExpense();
@@ -52,6 +52,45 @@ class UI {
     submitExpenseForm() {
         const expenseValue = this.expenseInput.value;
         const amountValue = this.amountInput.value;
+        if(expenseValue === '' || amountValue === '' || amountValue < 0) {
+            this.expenseFeedback.classList.add('showItem');
+            this.expenseFeedback.innerHTML = `<p>Values cannot be empty or negative</p>`
+            const self = this;
+            setTimeout(() => {
+                self.expenseFeedback.classList.remove('showItem')
+            }, 4000);
+        }
+        else {
+            let amount = parseInt(amountValue);
+            this.expenseInput = '';
+            this.amountInput = '';
+
+            let expense = {
+                id: this.itemID,
+                title: expenseValue,
+                amount: amount
+            };
+            this.itemID++;
+            this.itemList.push(expense);
+            this.addExpense(expense)
+        }
+    }
+    addExpense(expense){
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+        <h6 class="expense-title mb-0 text-uppercase list-item">-   ${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+        <div class="expense-icons list-item">
+         <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+          <i class="fas fa-edit"></i>
+         </a>
+         <a href="#" class="delete-icon" data-id="${expense.id}">
+          <i class="fas fa-trash"></i>
+         </a>
+        </div>
+       </div>`;
+       this.expenseList.appendChild(div);
     }
 
     totalExpense(){
